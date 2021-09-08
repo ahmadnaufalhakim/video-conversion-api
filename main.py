2e8e37e28e5876dd8069b7638e19e533a5df8812
ahmadnaufalhakim <ahmadnaufalhakim@gmail.com>
2021-09-08 11:28:15 +0700

    remove old uploaded and output files

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from controller import convert_video
 from flask import Flask, render_template, request, flash, redirect, url_for, send_file
 
 UPLOAD_FOLDER = "./uploads"
+OUTPUT_FOLDER = "./outputs"
+
 app = Flask(__name__)
 app.secret_key = "$3cR3t_K3y"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
-        
+app.config["OUTPUT_FOLDER"] = OUTPUT_FOLDER
+
 if not os.path.exists(app.config["UPLOAD_FOLDER"]) :
     os.makedirs(app.config["UPLOAD_FOLDER"])
 
@@ -25,8 +28,23 @@ def convert() :
         input_video_path = os.path.join(app.config["UPLOAD_FOLDER"], input_video.filename)
         input_video.save(input_video_path)
 
+        # Get the desired output from "output-format" dropdown options
         output_format = request.form.get("output-format")
+
+        # Remove all old output videos
+        for filename in os.listdir(app.config["OUTPUT_FOLDER"]) :
+            file_path = os.path.join(app.config["OUTPUT_FOLDER"], filename)
+            try :
+                os.unlink(file_path)
+            except Exception as e :
+                print("An error has occurred. %s" %(e))
+
+        # Convert video using the function in controller module
         output_video = convert_video(input_video_path, output_format)
+
+        # Remove all old uploaded videos
+        os.unlink(input_video_path)
+
         return send_file(output_video)
     else :
         return render_template("home.html")
